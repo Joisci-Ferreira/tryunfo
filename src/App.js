e3952baca172c1eb5b98eb44a0276d82cd7370c8
Joiscilene <joisci.lene@hotmail.com>
2022-02-25 15:47:13 -0300

    Adicionando validação

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.validateButton = this.validateButton.bind(this);
   }
 
   onInputChange({ target }) {
@@ -27,7 +28,7 @@ class App extends React.Component {
     const value = (target.type === 'checkbox') ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.validateButton);
   }
 
   onSaveButtonClick(event) {
@@ -40,6 +41,46 @@ class App extends React.Component {
       cardAttr2: 0,
       cardAttr3: 0,
       cardRare: 'normal',
+    });
+  }
+
+  validateInputs() {
+    const { cardName, cardDescription, cardImage } = this.state;
+    if (
+      cardName === ''
+    || cardDescription === ''
+    || cardImage === ''
+    ) { return false; }
+    return true;
+  }
+
+  validateAttributes() {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const maxNumber = 90;
+    const minNumber = 0;
+    const maxSum = 210;
+    const attr1 = Number(cardAttr1);
+    const attr2 = Number(cardAttr2);
+    const attr3 = Number(cardAttr3);
+    const sum = attr1 + attr2 + attr3 > maxSum;
+    if (
+      sum
+    || attr1 > maxNumber
+    || attr2 > maxNumber
+    || attr3 > maxNumber
+    || attr1 < minNumber
+    || attr2 < minNumber
+    || attr3 < minNumber
+    ) { return false; }
+    return true;
+  }
+
+  validateButton() {
+    const input = this.validateInputs();
+    const attribute = this.validateAttributes();
+    const buttonDisabled = input && attribute;
+    this.setState({
+      isSaveButtonDisabled: !buttonDisabled,
     });
   }
 
